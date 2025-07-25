61f96269e87d15a0327fc1ebcb3ac00d18ba13ca
HALFBLOOD7 <68768163+HALFBLOOD7@users.noreply.github.com>
2025-07-25 10:49:30 +0530

    Delete appGit directory

--- appGit/config.py
+++
@@ -1,8 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
-SECRET_KEY = os.getenv("SECRET_KEY", "your-default-secret-key")
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
